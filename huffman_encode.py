34df4f87c2597440bda0b30a53ca83cbe03170ac
SPatelC9 <sunnypatelc9@gmail.com>
2021-11-25 17:36:33 -0600

    Redid tree print. Made it more clearer, and added more comments for clarity. Final commit.

--- huffman_encode.py
+++ huffman_encode.py
@@ -62,6 +62,7 @@ def leftChild(index):
     return (2 * index) + 1
 
 
+# Huffman algorithm:
 def huffman(heap, tree):  # O(Nlog(N))
     if heap.size > 1:  # (N)
         first_min_value = heap.extract_min()
@@ -70,21 +71,23 @@ def huffman(heap, tree):  # O(Nlog(N))
         huffman(heap, tree)
 
 
-def merge_nodes(node1, node2, heap, tree):
+def merge_nodes(node1, node2, heap, tree):  # Takes a heap, tree and 2 node objects as parameter
     node1.bit = '0'
     node2.bit = '1'
     value = (node1.data[0] + node2.data[0], node1.data[1] + node2.data[1])
-    heap.insert(tree.Node(data=value, left=node1, right=node2, bit=None, traverse=False))
+    heap.insert(tree.Node(data=value, left=node1, right=node2, bit=None, traverse=False))  # Insert new node to heap
     heap.build_min_heap()  # log(N). build_min_heap takes log(N) time
 
 
-def traverse_tree(root):  # Post-Order Traversal.
+def traverse_tree(root):  # Post-Order Traversal. Takes in a node object as parameter
     if root:
         traverse_tree(root.left)
         traverse_tree(root.right)
         print(root.data, 'data', root.bit, 'bit')
 
 
+# bit_traversal function maps letter to code to and inserts it to bit_dictionary
+# The bits parameter is an array with all the bits leading up to a leaf node.
 def bit_traversal(node, bits, tree, bit_dictionary):
     if node and node is tree.root:  # Since the root does not have a bit, we make a separate if clause
         bit_traversal(node.left, bits, tree, bit_dictionary)
@@ -95,13 +98,13 @@ def bit_traversal(node, bits, tree, bit_dictionary):
         bit_traversal(node.right, bits, tree, bit_dictionary)
         if not node.left and not node.right:  # If it's a leaf node
             bit_dictionary[node.data[0]] = "".join(bits)  # maps the letter to code. (letter: code)
-            del bits[-1]   # Delete most recent added bit, for when we recurse back to parent and go to next child
+            del bits[-1]  # Delete most recent added bit, for when we recurse back to parent and go to next child
             node.traverse = True
         if node.left and node.right:  # If it's a parent node
             if node.left.traverse and node.right.traverse:  # Means we have visited both subtrees of a node
-                node.traverse = True  # Mark the parent of both subtrees as 2
+                node.traverse = True  # Mark the parent if all children subtrees have been traversed
                 if len(bits) >= 1:
-                    del bits[-1]
+                    del bits[-1]  # Delete the parent node bit for when we recurse back
 
 
 def file_read(file, hash_map):
@@ -116,6 +119,10 @@ def file_read(file, hash_map):
     file.close()
 
 
+# Takes the bit dictionary, which maps letter to code and writes the code aspect to a user requested file
+# bit_dictionary has the letter to code dictionary
+# r is the original file user wanted encoded
+# w is the output file user wants to redirect output of encoding process to. W later used for decoding
 def second_file_read(bit_dictionary, r, w):
     while True:
         character = r.read(1)
@@ -126,26 +133,37 @@ def second_file_read(bit_dictionary, r, w):
                 w.write(str(bit_dictionary[character]))
 
 
+# decoder function decodes the output of the encoding process
+# input file is the w file from second_file_read
+# tree consists of the root node only. Uses pointers to traverse nodes
+# output_file is the desired user file
+# decoder recursively iterates through heap, until we hit a leaf node, which we know is a character
+# then it recurses back to root when we hit a leaf and write character to output file,
+# since we don't know where the next character of the input file is located on the tree
+
 def decoder(node, input_file, tree, output_file):
     if node and not node.right and not node.left:
-        output_file.write(node.data[0])
-        decoder(tree.root, input_file, tree, output_file)
+        output_file.write(node.data[0])  # write the character the user output file
+        decoder(tree.root, input_file, tree, output_file)  # recurse back to root
     elif node:
-        character = input_file.read(1)
+        character = input_file.read(1)  # we read the encoded file and determine if it is a '0' or a '1'
         if character:
             if character == "0":
-                decoder(node.left, input_file, tree, output_file)
+                decoder(node.left, input_file, tree, output_file)  # recall nodes with bit 0 are placed on left side
             elif character == "1":
-                decoder(node.right, input_file, tree, output_file)
+                decoder(node.right, input_file, tree, output_file)  # recall nodes with bit 1 are placed on right side
 
 
-def printTree(node, level=0):
+def printTree(node, tree, level=0):  # Uses the idea of inorder traversal to print out tree. Prints tree horizontally
     if node:
-        printTree(node.left, level + 1)
+        string = 'root:'
+        if node is not tree.root:
+            string = '---{'
+        printTree(node.left, tree, level + 1)
         print('')
-        print(' ' * 6 * level + '->', node.data)
+        print(' ' * 5 * level + string, node.data)  # Adds spacing depending on what level node is
         print('')
-        printTree(node.right, level + 1)
+        printTree(node.right, tree, level + 1)
 
 
 def menu_print():
@@ -230,12 +248,36 @@ def main():
             w.close()
             r2.close()
             print("")
-            print("Your dictionary of code words in no particular order, is: \n")
+            print("")
+            print("TREE PRINT:")
+            print("---------------------------------------------------------------------------")
+            print("READ IN HORIZONTAL DIRECTION, LEFTMOST IS THE ROOT, RIGHTMOST IS LEAF\n"
+                  "EVERYTHING BELOW 'root:' IS THE LEFT-HAND SUBTREE OF ROOT\n"
+                  "EVERYTHING ABOVE 'root:' IS THE RIGHT-HAND SUBTREE OF ROOT"
+                  )
+            print("")
+            printTree(tree.root, tree)
+            print("")
+            print("SCROLL UP FOR TREE PRINT UNTIL YOU REACH 'root:' \n"
+                  "READ IN HORIZONTAL DIRECTION, LEFTMOST IS THE ROOT, RIGHTMOST IS LEAF\n"
+                  "EVERYTHING BELOW 'root:' IS THE LEFT-HAND SUBTREE OF ROOT\n"
+                  "EVERYTHING ABOVE 'root:' IS THE RIGHT-HAND SUBTREE OF ROOT")
+            print("")
+            print("")
+            print("PRINTED DICTIONARY OF CODE WORDS")
+            print("------------------------------------")
+            print("Your dictionary of codewords (letter to code) in NO PARTICULAR ORDER, is: \n")
             print(bit_dictionary)
+            print("")
+            print("")
+
+            print("SCROLL UP FOR TREE PRINT")
 
         except FileNotFoundError:
             msg = "Sorry, cannot find file. Please check your path to file and try again!"
             print(msg)
+    else:
+        print("Try again and enter valid option")
 
 
 if __name__ == "__main__":
